3e70615641cbb773dca4bb6edc381731cf6ba915
黄飞鹏 <1094521084@qq.com>
2023-02-02 17:59:27 +0800

    feat:引入eruda

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,13 @@
     <router-view />
 </template>
 <script setup lang="ts">
+/* eruda */
+import eruda from 'eruda'
 import { useLocale } from '@/i18n/use-locale'
 import { useEMConnectListener } from '@/EaseIM/hooks'
 useEMConnectListener()
 useLocale()
+eruda.init()
 </script>
 <style lang="scss">
 @import '@/assets/reset.css';
