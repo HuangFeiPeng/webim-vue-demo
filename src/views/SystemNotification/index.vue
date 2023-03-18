cffa08ec5ab0dae787f2abb23485e09fc9d8654f
黄飞鹏 <1094521084@qq.com>
2023-03-18 12:36:31 +0800

    feat:系统通知部分接口

--- src/views/SystemNotification/index.vue
+++ src/views/SystemNotification/index.vue
@@ -1,11 +1,34 @@
 <template>
     <!-- 顶部安全区 -->
     <div class="van-safe-area-top"></div>
-    <div class="systemnotifi_container"></div>
+    <div class="systemnotifi_container">
+        <div class="systemnotifi_container_card" v-for="(item, index) in systemNotfiList" :key="item.time + index">
+            <div class="time">{{ item.time }}</div>
+            <div class="card_body">
+                <div class="card_content">
+                    <img class="avatar" src="@/assets/images/login/loginIcon.png" alt="" />
+                    <div class="content">
+                        <div>{{ item.from }}</div>
+                        <p>{{ item.content }}</p>
+                    </div>
+                </div>
+                <div class="card_footer">
+                    <div>同意</div>
+                    <div>拒绝</div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+import { useSystemNotfiStore } from '@/stores'
 
-<style scoped lang="scss">
+const systemNotfiStore = useSystemNotfiStore()
+const systemNotfiList = computed(() => systemNotfiStore.systemNotificationList)
+</script>
+
+<style lang="scss" scoped>
 @import './index.scss';
 </style>
