a1add152312668eb36cab0b764916623a5c130e1
黄飞鹏 <1094521084@qq.com>
2023-03-23 18:33:25 +0800

    升级vue版本为解决生成默认的组件名,为做组件缓存做准备.

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,12 +15,14 @@ module.exports = {
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-        'vue/multi-word-component-names': [
-            //关闭vue组件不容许index.vue命名的校验
-            'error',
-            {
-                ignores: ['index'], //需要忽略的组件名
-            },
-        ],
+        // 'vue/multi-word-component-names': [
+        //     //关闭vue组件不容许index.vue命名的校验
+        //     'error',
+        //     {
+        //         ignores: ['index'], //需要忽略的组件名
+        //     },
+        // ],
+        //完全关闭组件名校验
+        'vue/multi-word-component-names': 'off',
     },
 }
