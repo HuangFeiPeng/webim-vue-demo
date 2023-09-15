deb2470fcfd987bfc09e4368cab16689a9a79649
黄飞鹏 <1094521084@qq.com>
2023-09-15 15:46:23 +0800

    modify:实例化SDK改为修改为函数调用.

--- src/IM/config/index.js
+++ src/IM/config/index.js
@@ -2,3 +2,7 @@
 export const DEFAULT_EASEMOB_APPKEY = 'easemob#easeim'
 export const DEFAULT_EASEMOB_SOCKET_URL = '//im-api-v2.easemob.com/ws'
 export const DEFAULT_EASEMOB_REST_URL = '//a1.easemob.com'
+export const CNNECTION_CUSTOM_CONFIG_KEY = 'EM_CONNECTION_CUSTOM_CONFIG'
+export const DEFAULT_EASEMOB_CONNECTIONS_OPTIONS = {
+    appKey: DEFAULT_EASEMOB_APPKEY
+}
--- src/IM/initwebsdk.js
+++ src/IM/initwebsdk.js
@@ -2,35 +2,40 @@
 // import EaseChatSDK from 'easemob-websdk'
 import EaseChatSDK from '@/SDK/Easemob-chat'
 import {
-    DEFAULT_EASEMOB_APPKEY,
-    DEFAULT_EASEMOB_SOCKET_URL,
-    DEFAULT_EASEMOB_REST_URL
+    DEFAULT_EASEMOB_CONNECTIONS_OPTIONS,
+    CNNECTION_CUSTOM_CONFIG_KEY
 } from './config'
-// 读取自定义配置（因demo需要自定义配置，非必须）
-const webimConfig = window.localStorage.getItem('webimConfig')
-const CUSTOM_CONFIG = (webimConfig && JSON.parse(webimConfig)) || {}
-console.log('>>>>>>webimConfig', CUSTOM_CONFIG)
+import _ from 'lodash'
+import { useLocalStorage } from '@vueuse/core'
+const EM_CONNECTION_CUSTOM_CONFIG = useLocalStorage(
+    CNNECTION_CUSTOM_CONFIG_KEY,
+    {}
+)
+console.log('EM_CONNECTION_CUSTOM_CONFIG', EM_CONNECTION_CUSTOM_CONFIG.value)
+let EaseChatClient = {}
+/**
+ * @param {object} options
+ * @example {appKey:'',isHttpDNS:false}
+ * @description 初始化环信客户端，如果传入了自定义配置则实例化传入的自定义配置
+ */
+const initEasemChatClient = (options) => {
+    if (
+        options &&
+        _.isObject(options) &&
+        Array.from(Object.keys(options)).length
+    ) {
+        EaseChatClient = new EaseChatSDK.connection(
+            _.assign(DEFAULT_EASEMOB_CONNECTIONS_OPTIONS, ...options)
+        )
+    } else {
+        EaseChatClient = new EaseChatSDK.connection(
+            _.assign(DEFAULT_EASEMOB_CONNECTIONS_OPTIONS)
+        )
+    }
+}
 
-//存放实例化后所有的方法
-// let EaseIMClient = {};
-// window.EaseIM = EaseIM = Easemob_SDK;
-//实例化环信SDK
-/*
- * isHttpDNS： isPrivate为true开启私有化配置则走自有配置的url以及apiUrl，
- * 否则为true就SDK自助获取DNS地址。
- * 【特别注意】如果不需要私有化配置，也就是自己定义url以及apiUrl。isHttpDNS、url、apiUrl，均可不用填写只用填入appKey！SDK内部会进行自动获取！
- **/
-const EaseChatClient = new EaseChatSDK.connection({
-    appKey: CUSTOM_CONFIG.appKey
-        ? CUSTOM_CONFIG.appKey
-        : DEFAULT_EASEMOB_APPKEY,
-    isHttpDNS: !CUSTOM_CONFIG.isPrivate, //取反isPrivate
-    url: CUSTOM_CONFIG.imServer
-        ? CUSTOM_CONFIG.imServer
-        : DEFAULT_EASEMOB_SOCKET_URL,
-    apiUrl: CUSTOM_CONFIG.restServer
-        ? `${CUSTOM_CONFIG.restServer}:${CUSTOM_CONFIG.port}`
-        : DEFAULT_EASEMOB_REST_URL,
-    enableLocalCache: true
-})
-export { EaseChatSDK, EaseChatClient }
+initEasemChatClient()
+if (Array.from(EM_CONNECTION_CUSTOM_CONFIG.value)) {
+    initEasemChatClient(EM_CONNECTION_CUSTOM_CONFIG.value)
+}
+export { EaseChatSDK, EaseChatClient, initEasemChatClient }
