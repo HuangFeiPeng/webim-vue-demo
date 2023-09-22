4985ed877bc09e2e26ffb9f82eb6eab39c4f1c6a
stwang <464905943@qq.com>
2023-09-22 10:54:11 +0800

    fixed: groupEvent INFORM_FROM is underfined

--- src/IM/listener/imGroupListener.js
+++ src/IM/listener/imGroupListener.js
@@ -1,11 +1,13 @@
 import { EaseChatClient } from '../initwebsdk'
 import store from '@/store'
+import { informType } from '@/constant'
 export const imGroupListener = () => {
     const submitInformData = (fromType, informContent) => {
         console.log('>>>submitInformData>>>', fromType, informContent)
         store.dispatch('createNewInform', { fromType, informContent })
     }
     const mountGroupEventListener = () => {
+        const { INFORM_FROM } = informType
         EaseChatClient.addEventHandler('groupEvent', {
             onGroupEvent: (groupevent) => {
                 console.log('>>>>>>>收到群组事件', groupevent)
